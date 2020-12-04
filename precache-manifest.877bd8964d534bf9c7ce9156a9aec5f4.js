2bcf0c44a907260edf904c0dc4c918d2b03cb0fb
Wladyslaw Ko <ever.at.work@gmail.com>
2020-12-04 17:11:39 +0200

    нас навчали зайвого не писать

---
+++ precache-manifest.877bd8964d534bf9c7ce9156a9aec5f4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6367368c99aabae9641a09683a1b5696",
+    "url": "/cotd/index.html"
+  },
+  {
+    "revision": "589e6d224b3e223b580d",
+    "url": "/cotd/static/css/main.69de8502.chunk.css"
+  },
+  {
+    "revision": "a3c17d8a0a5df694a1c1",
+    "url": "/cotd/static/js/2.d66b57da.chunk.js"
+  },
+  {
+    "revision": "b73db60337a63448fc0f0ed577e37ab5",
+    "url": "/cotd/static/js/2.d66b57da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "589e6d224b3e223b580d",
+    "url": "/cotd/static/js/main.d186e9f3.chunk.js"
+  },
+  {
+    "revision": "10dad5fa493e8dda56cf",
+    "url": "/cotd/static/js/runtime-main.947e19b4.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/cotd/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/cotd/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/cotd/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/cotd/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "c175fe49ca6c599199a81c6791490344",
+    "url": "/cotd/static/media/blanch_caps_inline-webfont.c175fe49.svg"
+  },
+  {
+    "revision": "47e26dee9b053b540478750bdf7a87d5",
+    "url": "/cotd/static/media/haymaker-webfont.47e26dee.svg"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/cotd/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/cotd/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/cotd/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
